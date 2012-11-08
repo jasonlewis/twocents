04ca9ddad3a9634d55dcd9892e0c37ddba1dc7d4
Jason Lewis <jason.lewis1991@gmail.com>
2012-11-08 20:50:01 +1100

    Fixed some styling issues and wrapping pages in article divs for correct styling.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- views/page.blade.php
+++ views/page.blade.php
@@ -1,5 +1,11 @@
-@if($page->title)
-	<h2 class="title">{{ $page->title }}</h2>
-@endif
+<div id="articles">
+	<div class="article">
+		@if($page->title)
+			<h2 class="title">{{ $page->title }}</h2>
+		@endif
 
-{{ $page->body }}
+		<div class="body">
+			{{ $page->body }}
+		</div>
+	</div>
+</div>
